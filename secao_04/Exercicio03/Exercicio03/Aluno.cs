fe3d6585c3e44244fe120f805910fc827eb9bab5
sousagomide <sousagomide@gmail.com>
2024-02-01 17:42:11 -0300

    update

---
+++ secao_04/Exercicio03/Exercicio03/Aluno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio03 {
+    class Aluno {
+
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+        public double Media;
+
+        public Aluno(double media) { 
+            this.Media = media;
+        }
+
+
+        public double NotaFinal() {
+            return this.Nota1 + this.Nota2 + this.Nota3;
+        }
+
+        public string Situacao() {
+            double nf = this.NotaFinal();
+            if (nf >= this.Media)
+                return "APROVADO";
+            else
+                return $"REPROVADO\nFALTARAM {(this.Media - nf).ToString("F2", CultureInfo.InvariantCulture)} PONTOS";
+        }
+    }
+}
---
+++ secao_04/Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio03 {
+    class Program {
+        static void Main(string[] args) {
+            Aluno a = new Aluno(60.0);
+            Console.Write("Nome do aluno: ");
+            a.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            a.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"NOTA FINAL = {a.NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine(a.Situacao());
+
+        }
+    }
+}
