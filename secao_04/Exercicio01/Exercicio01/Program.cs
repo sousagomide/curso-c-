5ac0d0fefb03a13f47af37231590d34ae153921c
sousagomide <sousagomide@gmail.com>
2024-02-01 14:59:07 -0300

    update

---
+++ secao_04/Exercicio01/Exercicio01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio01 {
+    class Program {
+        static void Main(string[] args) {
+            Retangulo r = new Retangulo();
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"AREA = {r.Area().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"PERÍMETRO = {r.Perimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {r.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ secao_04/Exercicio01/Exercicio01/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio01 {
+    class Retangulo {
+
+        public double Largura;
+        public double Altura;
+
+        public double Area() {
+            return this.Largura * this.Altura;
+        }
+
+        public double Perimetro() {
+            return 2.0 * (this.Largura + this.Altura);
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt(Math.Pow(this.Largura, 2) + Math.Pow(this.Altura, 2));
+        }
+
+        public override string ToString() {
+            return $"Retângulo => Largura: {this.Largura}; Altura: {this.Altura}";
+        }
+
+    }
+}
