50acfe79d8bf6a9c459eab7a23c1950fd4170b69
sousagomide <sousagomide@gmail.com>
2024-02-05 15:04:00 -0300

    update

---
+++ secao_05/Banco/Banco/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco {
+    class Conta {
+        private double _saldo;
+        public string Numero { get; private set; }
+        public string Nome { get; set; }
+        public double DepositoInicial { get; set; }
+        
+        public Conta(string numero, string nome) {
+            Numero = numero;
+            Nome = nome;
+        }
+
+        public Conta(string numero, string nome, double depositoInicial) : this(numero, nome) {
+            DepositoInicial = depositoInicial;
+            _saldo = DepositoInicial;
+        }
+
+        public void deposito(double valor) {
+            _saldo += valor;
+        }
+
+        public void saque(double valor) {
+            _saldo -= (valor + 5.00);
+        }
+
+        public override string ToString() {
+            return $"Conta {Numero}, Titular: {Nome}, Saldo: $ {_saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ secao_05/Banco/Banco/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Entre o número da conta: ");
+            string numero = Console.ReadLine();
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string resposta = Console.ReadLine();
+            Conta c = null;
+            if (resposta == "s") {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(numero, nome, depositoInicial);
+            } else {
+                c = new Conta(numero, nome);
+            }
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c);
+            Console.Write("Entre um valor para depósito: ");
+            c.deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
+            Console.Write("Entre um valor para saque: ");
+            c.saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c);
+        }
+    }
+}
