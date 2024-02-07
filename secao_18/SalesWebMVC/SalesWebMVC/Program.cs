3b5a6a3b6b8d516dc0e3a3f5982010f4bb479e67
sousagomide <sousagomide@gmail.com>
2024-02-07 11:13:25 -0300

    update

--- secao_18/SalesWebMVC/SalesWebMVC/Program.cs
+++ secao_18/SalesWebMVC/SalesWebMVC/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<SeedingService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +18,9 @@ if (!app.Environment.IsDevelopment()) {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var myService = builder.Services.BuildServiceProvider().GetService<SeedingService>();
+myService.Seed();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
